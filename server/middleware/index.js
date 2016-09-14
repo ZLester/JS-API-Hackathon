e70a7be1519dd0b79e1c25f33a5663a07ce011ee
Zachary Lester <zachary.greg.lester@gmail.com>
2016-09-14 11:24:35 -0700

    Add viewCounter middleware

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,7 +1,19 @@
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 
-module.exports = function(app) {
+// Our custom viewCounter middleware
+var viewCounter = function () {
+  var views = 0;
+  return function (req, res, next) {
+    // Attach the incremented view count to the request object...
+    req.views = ++views;
+    // ...and pass the request onto the next middleware.
+    next();
+  };
+};
+
+module.exports = function (app) {
+  // TODO: Mount our custom viewCounter middleware
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(morgan('dev'));
