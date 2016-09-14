a78a139284b67d6469aa56d3c96fd7ac3d9003a2
Zachary Lester <zachary.greg.lester@gmail.com>
2016-09-14 11:24:51 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,14 +12,18 @@ mongoose.connect(dbUri);
 
 var characterRouter = require('./routers/characterRouter');
 
-app.get('/', function(req, res) {
-  res.json({ message: 'HR Hackaton Example API' });
+app.get('/', function (req, res) {
+  res.json({
+    message: 'Welcome to the JS RESTful API Hackathon!',
+    // TODO: Mount our custom viewCounter in ./middleware so req.views isn't undefined
+    views: req.views
+  });
 });
 
 // TODO: Use the characterRouter as middleware on the '/api/characters' route
 
-app.listen(port, function() {
-  console.log('HR Hackathon Example API listening on ' + port);
-});  
+app.listen(port, function () {
+  console.log('JS API Hackathon listening on ' + port);
+});
 
 module.exports = app;
